55ffa67ceec50660f24b5079d0209bd26d018d9b
murilopaixao <murilopaixao@gmail.com>
2023-12-02 18:47:06 -0300

    inicio

---
+++ scripts.js
@@ -0,0 +1,25 @@
+var li_links = document.querySelectorAll(".links ul li")
+var view_wraps = document.querySelectorAll(".view_wrap")
+
+li_links.forEach(function(link){
+    link.addEventListener("click", function(){
+        li_links.forEach(function(item){
+            item.classList.remove("active");            
+        })
+
+        link.classList.add("active");
+
+        var li_view = link.getAttribute("data-view");
+
+        view_wraps.forEach(function(view){
+            view.style.display = "none";
+        })
+        
+        if(li_view == "list-view"){
+            document.querySelector("." + li_view).style.display = "block";
+        }
+        else {
+            document.querySelector("." + li_view).style.display = "block";
+        }
+    })
+})
